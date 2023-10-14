2785d8a86e9af553990708bb76016071af7cf04f
Madineto <uokeyezenekwe@yahoo.com>
2023-10-14 21:38:42 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import './ukwoma.css'
+import Button from './Button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      <hi> Hello, WELCOME TO THE NEW REACT!!!</hi>
+             <div> Devtown
+             </div>
+             <Button />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ukwoma.css';
+
+const Button = () => {
+  return (
+    <div className='second'>
+
+    <button type ='button'> Click Here </button>
+    </div>
+  )
+};
+
+export default Button
