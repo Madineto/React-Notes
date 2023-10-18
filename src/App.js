10da899c4001774d25caad7ac9fae7e636c9fc03
Madineto <uokeyezenekwe@yahoo.com>
2023-10-18 13:34:52 +0100

    2 days

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
-import Header from './Header';
-import Name from './Name';
-
-
+import Gallery from './Profile';
+// import Welcome from './Welcome';
+// import Wishes from './Wishes';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, Welcome to my Town!!!!</h1>
-      <Header />
-      <Name />
+      {/* <h1>Hello, Welcome to my Town!!!!</h1>
+      <Wishes name = "Janmes" hobby= " Driving">
+      <p> Hey You... whats good?</p>
+      <button> Sign in </button>
+      </Wishes>
+      
+      <Wishes name= "Janmes"  hobby= " Driving">
+      <button> click here</button>
+      </Wishes>
+      <Wishes name= "Janmes" hobby= " Driving" />
+      <Welcome name = " Joey" hobby = " fishing" /> */}
+
+   <Gallery />
+      
     </div>
   );
-}
+  }
 
 export default App;
