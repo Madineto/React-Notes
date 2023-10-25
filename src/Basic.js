938e275d352cee1b6b473c2a3a8e4a42e4ae7c8c
Madineto <uokeyezenekwe@yahoo.com>
2023-10-25 21:17:24 +0100

    user react

--- src/Basic.js
+++ src/Basic.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 
 const Basic =() => {
 
   const[message, setMessage] = useState('You again! Am tired.');
+  useEffect(() => {
+    console.log('Staggering Drunkard')
+
+    setTimeout(() =>{
+      setMessage("We Outside!!!")
+      }, 1500)
+  })
   return <h1>
  { message}
 
